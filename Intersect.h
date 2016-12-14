6e45492138c09e8ed737e8ebb17bb552645d4e7b
tabochans <tabochan931@gmail.com>
2016-12-14 14:52:13 +0900

    invmat

--- Intersect.h
+++ Intersect.h
@@ -587,7 +587,7 @@ namespace Isc{
 
 
 		float invmat[3][3] = {
-			{ mat[1][1] * mat[2][2] - mat[0][2] * mat[2][1], mat[0][2] * mat[2][1] - mat[0][1] * mat[2][2], mat[0][1] * mat[1][2] - mat[0][2] * mat[1][1] },
+			{ mat[1][1] * mat[2][2] - mat[1][2] * mat[2][1], mat[0][2] * mat[2][1] - mat[0][1] * mat[2][2], mat[0][1] * mat[1][2] - mat[0][2] * mat[1][1] },
 			{ mat[1][2] * mat[2][0] - mat[1][0] * mat[2][2], mat[0][0] * mat[2][2] - mat[0][2] * mat[2][0], mat[0][2] * mat[1][0] - mat[0][0] * mat[1][2] },
 			{ mat[1][0] * mat[2][1] - mat[1][1] * mat[2][0], mat[0][1] * mat[2][0] - mat[0][0] * mat[2][1], mat[0][0] * mat[1][1] - mat[0][1] * mat[1][0] }
 		};
